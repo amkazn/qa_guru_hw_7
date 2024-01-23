323694f606d29e1e52507164f5ed6556c7bf232f
amkazn <amkazn@gmail.com>
2024-01-23 15:54:05 +0300

    to review

--- src/test/java/pages/TextBoxPage.java
+++ src/test/java/pages/TextBoxPage.java
@@ -1,7 +1,6 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.conditions.Value;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
